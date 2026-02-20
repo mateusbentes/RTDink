1649689dc6527c539145a52692275cbd25916760
Mateus Sales Bentes <mateusbentes@gmail.com>
2026-02-20 16:16:54 -0300

    Fix g_bIsFullScreen: define only on PLATFORM_OSX, not Linux (SDL2Main.cpp owns it there)

--- source/App.cpp
+++ source/App.cpp
@@ -42,7 +42,7 @@ void AddText(const char *tex, const char *filename);
 #include "StackWalker/StackUtils.h"
 #endif
 
-#if defined(RTLINUX) || defined(PLATFORM_LINUX) || defined(PLATFORM_OSX)
+#ifdef PLATFORM_OSX
 bool g_bIsFullScreen = false;
 #else
 extern bool g_bIsFullScreen;
